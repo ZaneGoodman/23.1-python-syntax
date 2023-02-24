bc607ca08b8512d77d53c8fcfad78da57fc69ff8
zanegoodman <zgoodman1776@gamil.com>
2023-02-24 09:11:07 -0500

    completed

---
+++ count_up.py
@@ -0,0 +1,20 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+    my_list = list(range(start, stop + 1))
+    print(my_list)
+
+
+count_up(5, 7)        
